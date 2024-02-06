f2dcdd24c44678c2ec84709f8d57d9d78f027360
Emmanuel ZIMMERT <ez@ladigitale.dev>
2024-02-06 20:29:32 +0100

    Update PHP files to use env var

--- inc/televerser_fichier_import.php
+++ inc/televerser_fichier_import.php
@@ -2,31 +2,7 @@
 
 session_start();
 
-$listeDomaines = '../../../domaines-autorises.txt';
-if (isset($_SESSION['domainesAutorises']) || file_exists($listeDomaines)) {
-	if (isset($_SESSION['domainesAutorises']) && $_SESSION['domainesAutorises'] !== '') {
-		$domainesAutorises = $_SESSION['domainesAutorises'];
-	} else if (file_exists($listeDomaines)) {
-		$domainesAutorises = file_get_contents($listeDomaines);
-		$_SESSION['domainesAutorises'] = $domainesAutorises;
-	}
-	$domainesAutorises = explode(',', $domainesAutorises);
-	$origine = $_SERVER['SERVER_NAME'];
-	if (in_array($origine, $domainesAutorises, true)) {
-		header('Access-Control-Allow-Origin: $origine');
-		header('Access-Control-Allow-Methods: POST');
-		header('Access-Control-Max-Age: 1000');
-		header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
-	} else {
-		header('Location: ../');
-		exit();
-	}
-} else {
-	header('Access-Control-Allow-Origin: *');
-	header('Access-Control-Allow-Methods: POST');
-	header('Access-Control-Max-Age: 1000');
-	header('Access-Control-Allow-Headers: Content-Type, X-Requested-With');
-}
+require 'headers.php';
 
 if (!empty($_FILES['blob']) && !empty($_POST['serie'])) {
 	$index = $_POST['index'];
